e2715b754806913bd85146d8e1e4f9e55de0264e
jeff mascarenhas <>
2022-04-09 18:44:49 -0300

    styles: add card style

--- containers/auth/screens/auth/auth.js
+++ containers/auth/screens/auth/auth.js
@@ -3,19 +3,21 @@ import styles from "./auth.module.css";
 const Auth = () => {
   return (
     <div className={styles.auth}>
-      <div className="text-center">
-        <h1>Welcome Back</h1>
-        <h2>The decentralized web awaits</h2>
-      </div>
-      <form className={styles["auth-box"]}>
-        <p>
-          <input placeholder="Password" />
-        </p>
-        <button className="primary">Unlock</button>
-      </form>
-      <div className={styles["auth-box"]}>
-        <button>+ Create account</button>
-        <button> {`>`} Import account</button>
+      <div className="card">
+        <div className="text-center">
+          <h1>Clone MetaMask</h1>
+          <h2>The decentralized web awaits</h2>
+        </div>
+        <form className={styles["auth-box"]}>
+          <p>
+            <input placeholder="Password" />
+          </p>
+          <button className="primary">Unlock</button>
+        </form>
+        <div className={styles["auth-box"]}>
+          <button>+ Create account</button>
+          <button> {`>`} Import account</button>
+        </div>
       </div>
     </div>
   );
